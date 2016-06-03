2a57050de0e93862626b1edd835319517bfc6ece
Erin Greenhalgh <erin.greenhalgh0113@gmail.com>
2016-06-03 13:04:05 -0600

    delete comments

--- app/models/node.rb
+++ app/models/node.rb
@@ -2,10 +2,6 @@ class Node < ActiveRecord::Base
   belongs_to :right, class_name: "Node"
   belongs_to :left, class_name: "Node"
 
-  #check the value of the root node vs the value to add
-  #if the value is less than the root node, add it as the left child
-  #if the value is greater than the root node, add it as the right child
-
 
   def insert(value)
     if value < self.value
@@ -24,8 +20,6 @@ class Node < ActiveRecord::Base
   end
 
   def count
-    #count starts at one which is the current node
-    #add to the node by going to the left or right if they are not nil
     count = 1
     count += self.left.count if self.left
     count += self.right.count if self.right
@@ -33,7 +27,6 @@ class Node < ActiveRecord::Base
   end
 
   def max
-    #keep going to the right until self.right is nil
     unless self.right
       self.value
     else
@@ -42,16 +35,10 @@ class Node < ActiveRecord::Base
   end
 
   def min
-    #keep going left until self.left is nil
     unless self.left
       self.value
     else
       self.left.min
-    end 
+    end
   end
-
-
-
-
-
 end
